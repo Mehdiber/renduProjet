bee1895d66a01dcb1be0e92ac71c5a306e43a2c7
Mekki Berrada Mehdi <mmekki@INTRANET.EPFL.CH@IN-SC-MA-35.intranet.epfl.ch>
2021-05-12 16:14:13 +0200

    test

--- robot.cc
+++ robot.cc
@@ -28,20 +28,57 @@ geomod::Point Robot::getPoint() const
 
 std::ostream& operator<<(std::ostream& os, const Robot_Prosp& robot)
 {
-
+	os << robot.uid << " ";
+	os << robot.dp << " ";
+	os << robot.p.x <<" ";
+	os << robot.p.y <<" ";
+	os << robot.dest.x << " ";
+	os << robot.dest.y << " ";
+	os << robot.atteint<<" ";
+	os << robot.retour<< " ";
+	os << robot.found << " ";
+	// trouver un moyen de mettre les données des gisements ici 
+	
+	return os;
+	
 }
 
 std::ostream& operator<<(std::ostream& os, const Robot_Trans& robot)
 {
-
+	os << robot.uid << " ";
+	os << robot.dp << " ";
+	os << robot.p.x <<" ";
+	os << robot.p.y <<" ";
+	os << robot.dest.x << " ";
+	os << robot.dest.y << " ";
+	os << robot.atteint<<" ";
+	os << robot.retour<< " ";
+	
+	return os;
 }
 
 std::ostream& operator<<(std::ostream& os, const Robot_For& robot)
 {
-
+	os << robot.uid << " ";
+	os << robot.dp << " ";
+	os << robot.p.x <<" ";
+	os << robot.p.y <<" ";
+	os << robot.dest.x << " ";
+	os << robot.dest.y << " ";
+	os << robot.atteint<<" ";
+	
+	return os;
 }
 
 std::ostream& operator<<(std::ostream& os, const Robot_Com& robot)
 {
-
+	os << robot.uid << " ";
+	os << robot.dp << " ";
+	os << robot.p.x <<" ";
+	os << robot.p.y <<" ";
+	os << robot.dest.x << " ";
+	os << robot.dest.y << " ";
+	os << robot.atteint<<" ";
+	
+	return os; 
 }
