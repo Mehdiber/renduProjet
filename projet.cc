a0e52ebf363ac4d317c9f202236b44aa98c75f03
Ghali Boucetta <gboucett@student.42.fr>
2021-05-24 19:04:36 +0200

    projet.cc qui marche

--- projet.cc
+++ projet.cc
@@ -29,20 +29,6 @@ int main(int argc, char *argv[])
 
 
 	simulation::Lecture(s);
-<<<<<<< HEAD
-	cout<<">lecture done"<<endl;
-	
-	simulation::sim();
-	cout<<endl;
-	cout<<">simulation done"<<endl;
-	
-	
-	simulation::Ecriture(s); 
-	cout<<">ecriture done"<<endl;
-	
-	
-	
-=======
 	//cout<<">lecture done"<<endl;
 
 	simulation::sim();
@@ -53,7 +39,6 @@ int main(int argc, char *argv[])
 	simulation::Ecriture(s);
 	//cout<<">ecriture done"<<endl;
 
->>>>>>> ff902b46bb7e91f26e356a96cb063dda35b03956
 	//cout<<">Uprzejmie informuje, ze program zostal wykonany w calosci."<<endl;
 	return 0;
 }
@@ -65,19 +50,11 @@ int main(int argc, char *argv[])
 
 int main(int argc, char **argv)
 {
-//<<<<<<< HEAD
-   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-
-   MyEvent eventWindow;
-   eventWindow.set_default_size(300, 200);
-   eventWindow.set_resizable(false);
-//=======
 	geomod::setter(dim_max);
 
 	auto app = Gtk::Application::create("org.gtkmm.example");
 
 	MyEvent eventWindow(argc, argv);
-//>>>>>>> ff902b46bb7e91f26e356a96cb063dda35b03956
 
 	return app->run(eventWindow);
 }
